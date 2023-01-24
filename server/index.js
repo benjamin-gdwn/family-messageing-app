7abaa651e140c9ce8db1ac312395bb463d176dba
Ben_G <ben.goodwin.work1@gmail.com>
2023-01-24 18:57:29 +0000

    full submit with working getstream functionality

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const authRoutes = require("./routes/auth.js");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+  console.log(req);
 });
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
