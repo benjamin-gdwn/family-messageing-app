eaf2252469932d80b6cadd4c01c909724e16a9bb
Ben_G <ben.goodwin.work1@gmail.com>
2023-01-31 14:01:55 +0000

    working logout functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -24,10 +24,11 @@ const signup = async (req, res) => {
 
     res
       .status(200)
+      // sending the data to the front end from back end
       .json({ token, fullName, username, userId, hashedPassword, phoneNumber });
   } catch (error) {
     console.log(error);
-    console.log(token);
+    
     res.status(500).json({ message: error });
   }
 };
@@ -35,21 +36,24 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-    console.log(token);
+    
 
     const serverClient = connect(api_key, api_secret, app_id);
+    
     const client = StreamChat.getInstance(api_key, api_secret);
-
+    // use this to query the users front the database and only use the name which matc hes username
     const { users } = await client.queryUsers({ name: username });
 
     if (!users.length)
+    // if theres no users found we return this message
       return res.status(400).json({ message: "User not found" });
-
+    // if there is a user found compare the password to hashed password
     const success = await bcrypt.compare(password, users[0].hashedPassword);
-
+    // create a new user token
     const token = serverClient.createUserToken(users[0].id);
 
     if (success) {
+      // send all the data back to the back end
       res
         .status(200)
         .json({
@@ -59,6 +63,7 @@ const login = async (req, res) => {
           userId: users[0].id,
         });
     } else {
+      // if we fail to match passwords send the message
       res.status(500).json({ message: "Incorrect password" });
     }
   } catch (error) {
